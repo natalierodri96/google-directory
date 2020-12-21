b47b199e7a6637a54f3cd770ee888b70029e44a2
Natalie Rodriguez <natalierodri96@gmail.com>
2020-12-20 23:02:01 -0600

    search

---
+++ google/src/Books.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Books from './Books';
+
+
+class Books extends Component {
+    render () {
+        return (
+            <div className="App">
+              <Books/>
+            </div>
+        );
+    }
+}
+
+
+export default Books;
---
+++ google/src/Search.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Search from './Search';
+
+
+class Search extends Component {
+    render () {
+        return (
+            <div className="App">
+              <Books/>
+            </div>
+        );
+    }
+}
+
+
+export default Search;
