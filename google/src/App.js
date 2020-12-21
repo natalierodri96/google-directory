f2a110959fcc5c10848a6a6f93806dae3846e398
Natalie Rodriguez <natalierodri96@gmail.com>
2020-12-20 22:37:37 -0600

    header

--- google/src/App.js
+++ google/src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import { Layout, Header, Navigation, Content } from 'react-mdl';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       
-      
-      </div>
+      <div className="demo-big-content">
+    <Layout>
+        <Header title="Google Book Search" scroll>
+            <Navigation>
+               
+            </Navigation>
+        </Header>
+        <Content>
+            <div className="page-content" />
+        </Content>
+    </Layout>
+</div>
     );
   }
 }
